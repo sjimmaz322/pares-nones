8da3c801867ee4b5b6cd0ea7b6570565b85936dc
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-11-02 12:36:30 +0100

    Nombre cambiado porque era la parte B del ejercicio

--- paresnones/src/main/java/sjimmaz322/ParteB.java
+++ paresnones/src/main/java/sjimmaz322/ParteB.java
@@ -1,12 +1,48 @@
-
 package sjimmaz322;
 
+import java.util.Random;
+import javax.swing.JOptionPane;
 
-public class ParteB {
+public class ParteA {
 
-    
     public static void main(String[] args) {
+
+        Random numAleatorio = new Random();
+        int numDedosMaquina = numAleatorio.nextInt(11);
+        int numDedos;
+        String numDedosText;
+        String selection;
+        int suma;
+
+        selection = JOptionPane.showInputDialog(null, "¿Jugamos a Pares y Nones?" + "\n"
+                + "Introduzca 1 para si" + "\n"
+                + "Introduzca 0 para no");
+        int option = Integer.parseInt(selection);
+        switch (option){
+            case 1:
+        numDedosText = JOptionPane.showInputDialog(null, "¿Cuántos dedos quieres sacar?");
+        numDedos = Integer.parseInt(numDedosText);
+        suma =numDedosMaquina + numDedos;
         
+        if (suma % 2 == 0) {
+            JOptionPane.showMessageDialog(null, "Ha salido pares" + "\n" + "Tú sacaste: " + numDedos + "\n"
+                    + "La máquina sacó: " + numDedosMaquina);
+
+        } else {
+            JOptionPane.showMessageDialog(null, "Ha salido nones" + "\n" + "Tú sacaste: " + numDedos + "\n"
+                    + "La máquina sacó: " + numDedosMaquina);
+        }
+            break;
+        
+            case 0:
+            JOptionPane.showMessageDialog(null, "Otra vez será");
+            break;
+            
+            default:
+            JOptionPane.showMessageDialog(null, "Las opciones válidas son 0 o 1");
+            break;
+        }
+
     }
-    
+
 }
