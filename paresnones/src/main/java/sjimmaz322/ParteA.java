ddd6447024d79dc01162f740b95cc9bc6f56eb9f
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-11-02 12:52:41 +0100

    Añadido bucle para repetir cuando no se seleccione una opción válida

--- paresnones/src/main/java/sjimmaz322/ParteA.java
+++ paresnones/src/main/java/sjimmaz322/ParteA.java
@@ -10,16 +10,28 @@ public class ParteA {
         
         int numDedosJugadorA;
         int numDedosJugadorB;
-        
-        
+        int apuesta;
+        do{
         String apuestaText = JOptionPane.showInputDialog(null, "¿Apuestas por Pares o por Nones?"+
                 "\n"+"1 para nones"+"\n"+"2 para pares");
-        int apuesta = Integer.parseInt(apuestaText);
+        apuesta = Integer.parseInt(apuestaText);
         
         switch (apuesta){
             case 1:
+                //JOption
+            break;
+            
+            case 2:
+                //JOption
+            break;
+            
+            default:
+                JOptionPane.showMessageDialog(null, "Los valores aceptables son 1 o 2");
+            break;
+                            
                 
         }
+        }while(!(apuesta==1 || apuesta==2));
         
     }
 
