137a1c5cdfcff6cd61a36d65bf8d159caf15fa4b
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-11-02 12:29:34 +0100

    Creado if-else para decidir si jugar o no

--- paresnones/src/main/java/sjimmaz322/ParteA.java
+++ paresnones/src/main/java/sjimmaz322/ParteA.java
@@ -1,31 +1,41 @@
-
 package sjimmaz322;
 
 import java.util.Random;
 import javax.swing.JOptionPane;
 
-
 public class ParteA {
 
-    
     public static void main(String[] args) {
-        
+
         Random numAleatorio = new Random();
         int numDedosMaquina = numAleatorio.nextInt(11);
+        int numDedos;
+        String numDedosText;
+        String selection;
+        int suma;
+
+        selection = JOptionPane.showInputDialog(null, "¿Jugamos a Pares y Nones?" + "\n"
+                + "Introduzca 1 para si" + "\n"
+                + "Introduzca 0 para no");
+        int option = Integer.parseInt(selection);
         
-        String numDedosText = JOptionPane.showInputDialog(null, "¿Cuántos dedos quieres sacar?");
-        int numDedos = Integer.parseInt(numDedosText);
+        if (option==1){
+        numDedosText = JOptionPane.showInputDialog(null, "¿Cuántos dedos quieres sacar?");
+        numDedos = Integer.parseInt(numDedosText);
+        suma =numDedosMaquina + numDedos;
         
-        int suma = numDedosMaquina + numDedos;
-        
-        if (suma%2 == 0){
-            JOptionPane.showMessageDialog(null, "Ha salido pares"+"\n"+"Tú sacaste: " + numDedos +"\n"+
-                    "La máquina sacó: " + numDedosMaquina);
-            
+        if (suma % 2 == 0) {
+            JOptionPane.showMessageDialog(null, "Ha salido pares" + "\n" + "Tú sacaste: " + numDedos + "\n"
+                    + "La máquina sacó: " + numDedosMaquina);
+
         } else {
-            JOptionPane.showMessageDialog(null, "Ha salido nones"+"\n"+"Tú sacaste: " + numDedos +"\n"+
-                    "La máquina sacó: " + numDedosMaquina);
+            JOptionPane.showMessageDialog(null, "Ha salido nones" + "\n" + "Tú sacaste: " + numDedos + "\n"
+                    + "La máquina sacó: " + numDedosMaquina);
         }
+        } else if (option==0){
+            JOptionPane.showMessageDialog(null, "Otra vez será");
+        }
+
     }
-    
+
 }
